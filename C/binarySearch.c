4aee71083f9842bf8ed8ee2913344c45e978ca87
riceset <riceset@icloud.com>
2021-03-03 15:41:19 -0300

    Binary Search

--- C/binarySearch.c
+++ C/binarySearch.c
@@ -1,40 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int arrayLen = 10;
+
+int search(int arr[], int target);
 
 int main(void)
 {
-	int numbers[arrayLen] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int arr[] = {2, 3, 4, 7, 8, 9, 11, 13};
+	int	target = get_int("Input: ");
 
-	int target = get_int("Enter your target: ");
-	bool present;
-	
-	int middleIndex = arrayLen / 2;
-	int middle = numbers[middleIndex] - 1;
-
-	for (int i = 0; i < 10; i++)
+	int result = search(arr, target);
+	if (result != -1)
 	{
-
-		if (numbers[i] != target)
-		{
-			present = false;
-		}	
-
-	}
-
-	if (middle == target)
-	{
-		present = true;
-	}
-	
-	if (present)
-	{
-		printf("The number is present!\n");
+		printf("Found.\n");
 	}
 
 	else
 	{
-		printf("The number isn't present...\n");
+		printf("Not found.\n");
 	}
+}
+
+int search(int arr[], int target)
+{
+	int left = 0;
+	int right = 8 - 1; //where 8 is the lenght of the array
+	
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		
+		if (arr[mid] == target)
+			return mid;
+		else if (target < arr[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return -1;
 }
