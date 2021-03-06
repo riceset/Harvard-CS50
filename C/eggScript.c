42d29488809556684ee0fa8af6426c3564e6dfc3
riceset <riceset@icloud.com>
2021-03-05 21:11:41 -0300

    removed emojis

--- C/eggScript.c
+++ C/eggScript.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
 
-int main() {
-	int amount;
-    
-    printf("🐓 Welcome to EggCount!\n");
-	printf("Enter the number of eggs for the day: ");
-	scanf("%i", &amount);
+int main()
+{
+    int amount;
+
+    printf("Enter the number of eggs for the day: ");
+    scanf("%i", &amount);
     printf("\n");
-	int dozen = amount / 12;
-	int remainder = amount % 12;
-    
-    if (amount == 1) {
+    int dozen = amount / 12;
+    int remainder = amount % 12;
+
+    if (amount == 1)
+    {
         printf("You have 1 egg.\n");
-        printf("🥚\n");
+        //printf("🥚\n");
     }
-    
-    else if (amount < 12) {
+
+    else if (amount < 12)
+    {
         printf("You have %i eggs.", amount);
-        
     }
-    
-    else if (amount == 12) {
+
+    else if (amount == 12)
+    {
         printf("You have a dozen.");
-        printf("🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚\n");
     }
-    
-    else if (amount % 12 == 0) {
+
+    else if (amount % 12 == 0)
+    {
         printf("You have %i dozens.", dozen);
     }
-        
-    
-    else if (dozen == 1 && remainder == 1) {
+
+    else if (dozen == 1 && remainder == 1)
+    {
         printf("You have a dozen and  1 egg.");
-        printf("🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚\n");
     }
-    
-    else if (dozen == 1 && remainder < 24) {
+
+    else if (dozen == 1 && remainder < 24)
+    {
         printf("You have a dozen and %i eggs.", remainder);
     }
-    
-    else if (remainder == 1) {
+
+    else if (remainder == 1)
+    {
         printf("You have %i dozens and 1 egg.", dozen);
     }
-    
-    else if (remainder != 0) {
+
+    else if (remainder != 0)
+    {
         printf("You have %i dozens and %i eggs.", dozen, remainder);
     }
 
-	return 0;
+    return 0;
 }
