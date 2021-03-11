6206274f2cd38a96b7f90c3317e44519cfdcd55e
riceset <riceset@icloud.com>
2021-03-10 22:45:36 -0300

    fibonacci

--- C/Concepts/Recursion/fibonacci.c
+++ C/Concepts/Recursion/fibonacci.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+int fib(int n);
+
 int main(void)
 {
+    int num;
+    printf("Number: ");
+    scanf("%i", &num);
 
+    int result = fib(num);
+    printf("result: %i\n", result);
 }
 
 int fib(int n)
 {
-    if (n)
-    {
-        
-    }
-
+    if (n == 1 || n == 0)
+        return n;
     else
-    {
-
-    }
+        return fib(n - 1) + fib(n - 2);
 }
